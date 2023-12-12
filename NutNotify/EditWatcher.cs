5caa776376cd035684e35a064dd460b963d959dc
onear233 <mifen2017@qq.com>
2023-12-12 23:07:05 +0800

    V0.1.1 231212

--- NutNotify/EditWatcher.cs
+++ NutNotify/EditWatcher.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace NutNotify
 {
     internal class EditWatcher
     {
+        public static string content;
+        public void init()
+        {
+            //监听路径
+            FileSystemWatcher watcher = new FileSystemWatcher(@"D:\sync\通知");
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            //设置监听的属性
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+            //绑定事件
+            watcher.Created += Watcher_Created;
+            //仅监听txt文件
+            watcher.Filter = "*.txt";
+        }
+
+        private void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+
+        }
+
+        private static void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+
+        }
+
+        private static void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            string fileName = $"{e.Name}";
+            string filePath = $"{e.FullPath}";
+
+            new Thread(() => {
+                Thread.Sleep(2000);
+                // 打开文件并创建 StreamReader 对象
+                StreamReader reader = new StreamReader(filePath);
+                // 读取文件内容
+                content = reader.ReadToEnd();
+
+                // 关闭流
+                reader.Close();
+                MainWindow.Instance.responseGetter(content);
+
+            }).Start();
+
+
+
+        }
+
     }
 }
--- NutNotify/MainWindow.xaml.cs
+++ NutNotify/MainWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +24,102 @@ namespace NutNotify
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public static MainWindow Instance { get; private set; }
+        private static Visibility _mainWindowVisibility;
+        private static string _notificationText;
+        public Visibility mainWindowVisibility
+        {
+            get
+            {
+                return _mainWindowVisibility;
+            }
+            set
+            {
+                _mainWindowVisibility = value;
+
+                RaisePropertyChanged("mainWindowVisibility");
+            }
+        }
+
+        public string notificationText
+        {
+            get
+            {
+                return _notificationText;
+            }
+            set
+            {
+                _notificationText = value;
+
+                RaisePropertyChanged("notificationText");
+            }
+        }
+
+        //public string mainWindowVisibility
+        //{
+        //    get
+        //    {
+        //        return mainWindowVisibility;
+        //    }
+        //    set
+        //    {
+        //        mainWindowVisibility = value;
+
+        //        RaisePropertyChanged("mainWindowVisibility");
+        //    }
+        //}
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            // 构造函数中为静态属性赋值
+            Instance = this;
+            EditWatcher watcher = new EditWatcher();
+            watcher.init();
+            //_mainWindowVisibility = Visibility.Hidden;
+            Visibility = Visibility.Hidden;
+        }
+        public static void bridgeForResponse(string value)
+        {
+            if(value != null)
+            {
+                
+            }
+        }
+
+        public void responseGetter(string value)
+        {
+            //_mainWindowVisibility = Visibility.Visible;
+            notificationTextBlock.Dispatcher.Invoke(() =>
+            {
+                notificationTextBlock.Text = value;
+            });
+            this.Dispatcher.Invoke(() =>
+            {
+                WindowState = WindowState.Normal;
+                this.Visibility = Visibility.Hidden;
+                this.Visibility = Visibility.Visible;
+
+            });
+            new ToastContentBuilder()
+                   .AddArgument("action", "viewConversation")
+                   .AddArgument("conversationId", 9813)
+                   .AddText("您有一条新消息")
+                   .AddText("打开主界面查看")
+                   .AddAudio(new Uri(Environment.GetEnvironmentVariable("SYSTEMROOT") + "\\Media\\Windows Ding.wav") )
+                   .Show();
         }
     }
 }
