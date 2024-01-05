961627124127753256f37cc7af6e1f31fc1d5f7e
onear233 <mifen2017@qq.com>
2024-01-05 17:40:15 +0800

    1.更新应用图标 2.默认退出程序后最小化到托盘

--- NutNotify/MainWindow.xaml.cs
+++ NutNotify/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MessageBox = System.Windows.MessageBox;
 using NutNotify.Pages;
+using System.Windows.Forms;
 
 namespace NutNotify
 {
@@ -34,6 +35,7 @@ namespace NutNotify
         public static MainWindow Instance { get; private set; }
         private static Visibility _mainWindowVisibility;
         private static string _notificationText;
+        private NotifyIcon notifyIcon;
         public Visibility mainWindowVisibility
         {
             get
@@ -93,8 +95,7 @@ namespace NutNotify
             Instance = this;
             EditWatcher watcher = new EditWatcher();
             watcher.init();
-
-            AutoUpdater.AppCastURL = "https://raw.gitmirror.com/onear233/NutNotify/master/updateInfo.xml";
+            showNotifyIcon();
             AutoUpdater.Start("https://raw.gitmirror.com/onear233/NutNotify/master/updateInfo.xml");
             //_mainWindowVisibility = Visibility.Hidden;
             // 初始化默认页面
@@ -103,16 +104,67 @@ namespace NutNotify
             //测试用
             //Visibility = Visibility.Hidden;
         }
-        public static void bridgeForResponse(string value)
+
+        private void showNotifyIcon()
         {
-            if(value != null)
+            this.notifyIcon = new NotifyIcon();
+            this.notifyIcon.Text = "NutNotify";
+            this.notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+            this.notifyIcon.Visible = true;
+            //打开菜单项
+            System.Windows.Forms.MenuItem open = new System.Windows.Forms.MenuItem("Open");
+            open.Click += new EventHandler(Show);
+            //退出菜单项
+            System.Windows.Forms.MenuItem exit = new System.Windows.Forms.MenuItem("Exit");
+            exit.Click += new EventHandler(Close);
+            //关联托盘控件
+            System.Windows.Forms.MenuItem[] childen = new System.Windows.Forms.MenuItem[] { open, exit };
+            notifyIcon.ContextMenu = new System.Windows.Forms.ContextMenu(childen);
+
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler((o, e) =>
             {
-                
-            }
+                if (e.Button == MouseButtons.Left) this.Show(o, e);
+            });
         }
 
+
+
+        private void Show(object sender, EventArgs e)
+        {
+            
+                this.Visibility = System.Windows.Visibility.Visible;
+                this.Activate();
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+        }
+
+        private void Hide(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        public static void bridgeForResponse(string value)
+        {
+            if (value != null)
+            {
+
+            }
+        }
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visibility = Visibility.Hidden;
+        }
         
-        
+
 
         private void navigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
